4b8d76c29a6e8380d5dbddf385f3810aabe5e07e
Daniel <Daniel@DESKTOP-VRHQRTG.home>
2015-12-10 22:17:52 -0200

    Tela de Cadastro e Serviço de Busca3

--- src/main/java/br/com/localizaja/controle/CadastroEstabelecimentoMB.java
+++ src/main/java/br/com/localizaja/controle/CadastroEstabelecimentoMB.java
@@ -2,12 +2,10 @@ package br.com.localizaja.controle;
 
 import br.com.localizaja.modelo.Empresa;
 import br.com.localizaja.modelo.GeoLocation;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
 import javax.faces.bean.ViewScoped;
 import lombok.Getter;
 import lombok.Setter;
@@ -32,7 +30,7 @@ public class CadastroEstabelecimentoMB {
         try {
 //            resultadoEnderecos = new ArrayList<GeoLocation>();
             resultadoEnderecos = Servico.getCoordenadasGeograficas(empresa.getEndereco() + " " + empresa.getCidade() + " " + empresa.getEndereco());
-            System.out.println("a");
+            System.out.println("eeend: " + empresa.getEndereco());
         } catch (Exception ex) {
             resultadoEnderecos = null; 
             Logger.getLogger(CadastroEstabelecimentoMB.class.getName()).log(Level.SEVERE, null, ex);
